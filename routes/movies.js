035cbc1de0422237d528a127b583495a7044d3d6
unknown <oswaldoj16@gmail.com>
2020-12-22 14:06:45 -0500

    firts commit including basic SCRUD system

---
+++ routes/movies.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const {moviesMock} = require('../utils/mocks/movies');
+
+function moviesApi(app){
+    const router = express.Router();
+    app.use("/api/movies",router);
+
+    router.get("/", async function(req,res,next){
+        try{
+            const movies = await Promise.resolve(moviesMock);
+
+            res.status(200).json({
+                data:movies,
+                message:'movies listed'
+            });
+        } catch(err){
+            next(err);
+        }
+    });
+    router.get("/:movieId", async function(req,res,next){
+        try{
+            const movies = await Promise.resolve(moviesMock[0]);
+
+            res.status(200).json({
+                data:movies,
+                message:'movies retrieved'
+            });
+        } catch(err){
+            next(err);
+        }
+    });
+    router.post("/", async function(req,res,next){
+        try{
+            const createdMovieId = await Promise.resolve(moviesMock[0].id);
+
+
+            res.status(201).json({
+                data: createdMovieId,
+                message:'movie created'
+            });
+        } catch(err){
+            next(err);
+        }
+    });
+    router.put("/:movieId", async function(req,res,next){
+        try{
+            const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data:updatedMovieId,
+                message:'movies updated'
+            });
+        } catch(err){
+            next(err);
+        }
+    });
+    router.delete("/:movieId", async function(req,res,next){
+        try{
+            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data:deletedMovieId,
+                message:'movies deleted'
+            });
+        } catch(err){
+            next(err);
+        }
+    });
+}
+
+module.exports = moviesApi;
